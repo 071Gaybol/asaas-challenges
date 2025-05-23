21b31fe5a3f663f2fd15857bee91cc544471e1b5
Gabriel Félix <gabriel.felix@asaas.com.br>
2025-05-23 12:23:17 -0300

    feat: add new table to split payers of clients and remove enums

--- SQL/modelagem-mini-asaas/db-create.sql
+++ SQL/modelagem-mini-asaas/db-create.sql
@@ -1,12 +1,19 @@
 /*passo 1: Criar o banco de dados*/
 CREATE SCHEMA mini_asaas;
 
-/*Passo 2: Criar a tabela de clientes*/
+/*Passo 2: Criar a tabela de clientes e pagadores*/
+CREATE TABLE pagadores (
+id INT AUTO_INCREMENT PRIMARY KEY,
+nome VARCHAR(255) ,
+cpf_cnpj VARCHAR(14),
+email VARCHAR(255),
+telefone VARCHAR(15),
+endereco VARCHAR(255));
+
 CREATE TABLE clientes (
 id INT AUTO_INCREMENT PRIMARY KEY,
-nome VARCHAR(255),
+nome VARCHAR(255) ,
 cpf_cnpj VARCHAR(14),
-tipo_cliente ENUM('pagador', 'dono', 'ambos'),
 email VARCHAR(255),
 telefone VARCHAR(15),
 endereco VARCHAR(255));
@@ -15,11 +22,11 @@ endereco VARCHAR(255));
 CREATE TABLE cobrancas (
 id INT AUTO_INCREMENT PRIMARY KEY,
 pagador_id INT,
-dono_id INT,
+cliente_id INT,
 valor DECIMAL(10,2),
 descricao VARCHAR(255),
 data_vencimento DATE,
-status ENUM('pendente', 'pago', 'cancelado'),
-FOREIGN KEY (pagador_id) REFERENCES clientes(id),
-FOREIGN KEY (dono_id) REFERENCES clientes(id)
+status VARCHAR(255),
+FOREIGN KEY (pagador_id) REFERENCES pagadores(id),
+FOREIGN KEY (cliente_id) REFERENCES clientes(id)
 );
