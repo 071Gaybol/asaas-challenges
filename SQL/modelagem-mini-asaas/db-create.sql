8097e0da1ee2869ccc23636228c04c9892d8082b
Gabriel Félix <gabriel.felix@asaas.com.br>
2025-05-27 12:07:51 -0300

    feat: create new person_type column and add NOT NULL on require columns

--- SQL/modelagem-mini-asaas/db-create.sql
+++ SQL/modelagem-mini-asaas/db-create.sql
@@ -4,16 +4,18 @@ CREATE SCHEMA mini_asaas;
 /*Passo 2: Criar a tabela de clientes e pagadores*/
 CREATE TABLE pagadores (
 id INT AUTO_INCREMENT PRIMARY KEY,
-nome VARCHAR(255) ,
-cpf_cnpj VARCHAR(14),
+nome VARCHAR(255) NOT NULL,
+cpf_cnpj VARCHAR(14) NOT NULL,
+person_type VARCHAR(255) NOT NULL,
 email VARCHAR(255),
 telefone VARCHAR(15),
 endereco VARCHAR(255));
 
 CREATE TABLE clientes (
 id INT AUTO_INCREMENT PRIMARY KEY,
-nome VARCHAR(255) ,
-cpf_cnpj VARCHAR(14),
+nome VARCHAR(255) NOT NULL,
+cpf_cnpj VARCHAR(14) NOT NULL,
+person_type VARCHAR(255) NOT NULL,
 email VARCHAR(255),
 telefone VARCHAR(15),
 endereco VARCHAR(255));
@@ -21,12 +23,12 @@ endereco VARCHAR(255));
 /*Passo 3: Criar a tabela de cobranças*/
 CREATE TABLE cobrancas (
 id INT AUTO_INCREMENT PRIMARY KEY,
-pagador_id INT,
-cliente_id INT,
-valor DECIMAL(10,2),
+pagador_id INT NOT NULL,
+cliente_id INT NOT NULL,
+valor DECIMAL(10,2) NOT NULL,
 descricao VARCHAR(255),
-data_vencimento DATE,
-status VARCHAR(255),
+data_vencimento DATE NOT NULL,
+status VARCHAR(255) NOT NULL,
 FOREIGN KEY (pagador_id) REFERENCES pagadores(id),
 FOREIGN KEY (cliente_id) REFERENCES clientes(id)
 );
